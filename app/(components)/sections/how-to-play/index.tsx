65fa9070ebca18068473c4c312bc63cebf2497a9
Tadashi <shinobi.design416@gmail.com>
2024-07-23 12:58:24 -0700

    Added display name to remove vercel deploy issue

--- app/(components)/sections/how-to-play/index.tsx
+++ app/(components)/sections/how-to-play/index.tsx
@@ -178,6 +178,8 @@ const CarouselSection = React.forwardRef<HTMLDivElement>((_, ref) => {
   );
 });
 
+CarouselSection.displayName = "CarouselSection";
+
 const BackgroundEffect: React.FC = () => {
   return (
     <div className="absolute -left-[100px] -top-[100px] w-full max-w-[550px] h-full max-h-[350px] blur-2xl opacity-30 rounded-full z-0 bg-primary" />
